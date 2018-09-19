17f2c028653b2c4a6fb53424d322dd3b4f3b33e1
devolasvegas <devondaviau@yahoo.ca>
2018-09-18 22:02:44 -0400

    initial commit

---
+++ app/routes/users.server.routes.js
@@ -0,0 +1,12 @@
+const users = require('../../app/controllers/users.server.controller');
+
+module.exports = function(app) {
+    app.route('/users')
+    .post(users.create)
+    .get(users.list);
+
+    app.route('/users/:userId')
+    .get(users.read);
+
+    app.param('userId', users.userByID);
+};
