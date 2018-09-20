17ce2c26a317e43b7d6240889b5026d3acc09ebc
devolasvegas <devondaviau@yahoo.ca>
2018-09-19 21:43:13 -0400

    add website property to user model and add getter modifier to the website property to update all documents when they are called

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -2,6 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+    website: {
+        type: String,
+        get: function(url) {
+            if(!url) {
+                return url;
+            } else {
+                if(url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
+                    url = 'http://' + url;
+                }
+
+                return url;
+            }
+        }
+    },
     firstName: String,
     lastName: String,
     email: String,
@@ -15,4 +29,7 @@ const UserSchema = new Schema({
         default: Date.now
     }
 });
+
+UserSchema.set('toJSON', { getters: true });
+
 mongoose.model('User', UserSchema);
