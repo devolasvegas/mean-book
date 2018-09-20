5c65f8d639ee6be0934da6897107b370405b479d
devolasvegas <devondaviau@yahoo.ca>
2018-09-19 21:51:28 -0400

    add virtual attribute fullName and its setter and getter methods

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -30,6 +30,17 @@ const UserSchema = new Schema({
     }
 });
 
-UserSchema.set('toJSON', { getters: true });
+UserSchema.virtual('fullName').get(function() {
+    return this.firstName + ' ' + this.lastName;
+}).set(function(fullName) {
+    const splitName = fullName.split(' ');
+    this.firstName = splitName[0] || '';
+    this.lastName = splitName[1] || '';
+});
+
+UserSchema.set('toJSON', {
+    getters: true,
+    virtuals: true
+});
 
 mongoose.model('User', UserSchema);
