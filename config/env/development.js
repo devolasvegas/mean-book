5e0dd0fbbba0d4e8dc00198043550f15e4fef2ad
devolasvegas <devondaviau@yahoo.ca>
2018-09-26 22:44:16 -0400

    remove facebook configuration

--- config/env/development.js
+++ config/env/development.js
@@ -1,9 +1,4 @@
 module.exports =  {
     db: 'mongodb://localhost/mean-book',
-    sessionSecret: 'developmentSessionSecret',
-    facebook: {
-        clientID: '2063070190427596',
-        clientSecret: '3b22603f7b001ebbbd36d48254eba10a',
-        callbackURL: 'http://localhost:3000/oauth/facebook/callback'
-    }
+    sessionSecret: 'developmentSessionSecret'
 }
