22b4d5c6da2721a075383defda216f9e96398620
devolasvegas <devondaviau@yahoo.ca>
2018-09-26 22:42:25 -0400

    Add facebook authentication configuration

--- config/env/development.js
+++ config/env/development.js
@@ -1,4 +1,9 @@
 module.exports =  {
     db: 'mongodb://localhost/mean-book',
-    sessionSecret: 'developmentSessionSecret'
+    sessionSecret: 'developmentSessionSecret',
+    facebook: {
+        clientID: '2063070190427596',
+        clientSecret: '3b22603f7b001ebbbd36d48254eba10a',
+        callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+    }
 }
