a5b9c65a1a4bb449ada978fd4ee5e74df4c3fab4
devolasvegas <devondaviau@yahoo.ca>
2018-10-04 21:39:22 -0400

    add transpiled files to Angular app and update index controller

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,14 +1,13 @@
 exports.render = function(req, res) {
 
-    if(req.session.lastVisit) {
-        console.log(req.session.lastVisit);
-    }
-
-    req.session.lastVisit = new Date();
+    const user = (!req.user) ? null : {
+        _id: req.user.id,
+        firstName: req.user.firstName,
+        lastName: req.user.lastName
+    };
     
     res.render('index', {
         title: 'Hello Worldzzz!',
-        heading: 'Well, ain\'t this some shit?',
-        userFullName: req.user ? req.user.fullName : ''
-    })
+        user: JSON.stringify(user);
+    });
 };
