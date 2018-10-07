baea43d7baea60dbfc890114e75e759bc0748328
devolasvegas <devondaviau@yahoo.ca>
2018-10-07 19:27:25 -0400

    update app component to include new signup and signup and authentication

--- public/app/app.component.js
+++ public/app/app.component.js
@@ -1,4 +1,4 @@
-System.register(["@angular/core"], function (exports_1, context_1) {
+System.register(["@angular/core", "@angular/router", "./authentication/authentication.service"], function (exports_1, context_1) {
     "use strict";
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -6,23 +6,35 @@ System.register(["@angular/core"], function (exports_1, context_1) {
         else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
         return c > 3 && r && Object.defineProperty(target, key, r), r;
     };
-    var core_1, AppComponent;
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var core_1, router_1, authentication_service_1, AppComponent;
     var __moduleName = context_1 && context_1.id;
     return {
         setters: [
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (router_1_1) {
+                router_1 = router_1_1;
+            },
+            function (authentication_service_1_1) {
+                authentication_service_1 = authentication_service_1_1;
             }
         ],
         execute: function () {
             AppComponent = /** @class */ (function () {
-                function AppComponent() {
+                function AppComponent(_authenticationService, router) {
+                    this._authenticationService = _authenticationService;
+                    this.router = router;
                 }
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'mean-app',
-                        template: '<h1>Hello World</h1>'
-                    })
+                        template: '<router-outlet></router-outlet>'
+                    }),
+                    __metadata("design:paramtypes", [authentication_service_1.AuthenticationService, router_1.Router])
                 ], AppComponent);
                 return AppComponent;
             }());
