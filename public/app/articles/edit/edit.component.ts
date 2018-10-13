47a18972b02f99fd7650cf0198927efbd51edd87
devolasvegas <devondaviau@yahoo.ca>
2018-10-13 13:03:22 -0400

    create edit component

---
+++ public/app/articles/edit/edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { ArticlesSErvice } from '../articles.service';
+
+@Component({
+    selector: 'edit',
+    templateUrl: 'app/articles/edit/edit.template.html'
+})
+export class EditComponent {
+    
+}
