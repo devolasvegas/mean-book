40009ebbf361ccd453c226e0e99ae7f2bdd6119c
devolasvegas <devondaviau@yahoo.ca>
2018-10-18 22:26:06 -0400

    pass db connection to express configuration method

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const configureExpress = require('./config/express');
 const configurePassport = require('./config/passport');
 
 const db = configureMongoose();
-const app = configureExpress();
+const app = configureExpress(db);
 const passport  = configurePassport();
 app.listen(3000);
 module.exports = app;
