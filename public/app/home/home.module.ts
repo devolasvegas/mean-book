630497c1922e16f727ba43ed3dfa88a5a4e7d634
devolasvegas <devondaviau@yahoo.ca>
2018-10-30 22:21:17 -0400

    import forms module

--- public/app/home/home.module.ts
+++ public/app/home/home.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomeRoutes } from './home.routes';
@@ -8,6 +9,7 @@ import { HomeComponent } from './home.component';
 @NgModule({
     imports: [
         CommonModule,
+        FormsModule,
         RouterModule.forChild(HomeRoutes)
     ],
     declarations: [
