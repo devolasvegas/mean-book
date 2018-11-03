7c9dca9367e7af98ab2c66d82383d2ca7a7ff30c
devolasvegas <devondaviau@yahoo.ca>
2018-11-02 20:08:26 -0400

    Create model unit tests

--- app/tests/article.server.model.tests.js
+++ app/tests/article.server.model.tests.js
@@ -5,3 +5,50 @@ const User = mongoose.model('User');
 const Article = mongoose.model('Article');
 
 let user, article;
+
+describe('Article Model Unit Tests: ', () => {
+    beforeEach((done) => {
+        user = new User({
+            firstName: 'Full',
+            lastName: 'Name',
+            displayName: 'Full Name',
+            email: 'test@test.com',
+            username: 'username',
+            password: 'password'
+        });
+
+        user.save(() => {
+            article = new Article({
+                title: 'Article Title',
+                content: 'Article content.',
+                user: user
+            });
+
+            done();
+        });
+    });
+
+    describe('Testing the save method', () => {
+        it('Should be able to save without problems', () => {
+            article.save((err) => {
+                should.not.exist(err);
+            });
+        });
+
+        it('Should not be able to save an article without a title', () => {
+            article.title = '';
+            
+            article.save((err) => {
+                should.exist(err);
+            });
+        });
+    });
+
+    afterEach((done) => {
+        Article.remove(() => {
+            User.remove(() => {
+                done();
+            });
+        });
+    });
+});
