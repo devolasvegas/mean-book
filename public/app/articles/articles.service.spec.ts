5296384c4e1c363fdef64afafe162cca0b0e6c70
devolasvegas <devondaviau@yahoo.ca>
2018-11-13 22:12:43 -0500

    build out test suite

--- public/app/articles/articles.service.spec.ts
+++ public/app/articles/articles.service.spec.ts
@@ -3,3 +3,53 @@ import { MockBackend, MockConnection } from '@angular/http/testing';
 import { HttpModule, Http, XHRBackend, Response, ResponseOptions } from '@angular/http';
 import { ArticlesService } from './articles.service';
 
+let backend: MockBackend;
+let service: ArticlesService;
+
+const mockArticle = {
+    title: 'An Article about MEAN',
+    content: 'MEAN Rocks!',
+    creator: {
+        fullName: 'John Doe'
+    }
+};
+
+describe('Articles service tests', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [ HttpModule ],
+            providers: [
+                ArticlesService,
+                {
+                    provide: XHRBackend,
+                    useClass: MockBackend
+                }
+            ]
+        })
+        .compileComponents();
+    }));
+
+    beforeEach(inject([Http, XHRBackend], (_http: Http, _mockBackend: MockBackend) => {
+        backend = _mockBackend;
+        service = new ArticlesService(_http);
+    }));
+
+    it('Should create a single article', done => {
+        const options = new ResponseOptions({
+            status: 200,
+            body: mockArticle
+        });
+        const response = new Response(options);
+
+        backend.connections.subscribe((connection: MockConnection) => connection.mockRespond(response));
+
+        service.create(mockArticle).do(article => {
+            expect(article).toBeDefined();
+
+            expect(article.title).toEqual(mockArticle.title);
+            expect(article.content).toEqual(mockArticle.component);
+
+            done();
+        }).toPromise();
+    });
+});
