77e4aa09303a248aaf401caae353fa6dc6d59f5f
devolasvegas <devondaviau@yahoo.ca>
2018-11-19 22:07:59 -0500

    add routes testing methods

--- public/app/app.routes.spec.ts
+++ public/app/app.routes.spec.ts
@@ -4,3 +4,35 @@ import { SpyLocation } from '@angular/common/testing';
 import { Location } from '@angular/common';
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { Router } from '@angular/router'; // This line appeared automatically when initializing router variable
+
+let router: Router;
+let location: SpyLocation;
+
+describe('AppComponent Routing', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [ AppModule, RouterTestingModule ]
+        }).compileComponents();
+    }));
+
+    beforeEach(fakeAsync(() => {
+        const injector = TestBed.createComponent(AppComponent).debugElement.injector;
+        location = injector.get(Location);
+    }));
+
+    it('Should navigate to home', fakeAsync(() => {
+        location.go('/');
+        expect(location.path()).toEqual('/');
+    }));
+
+    it('Should navigate to signin', fakeAsync(() => {
+        location.go('/authentication/signin');
+        expect(location.path()).toEqual('/authentication/signin');
+    }));
+
+    it('Should navigate to signup', fakeAsync(() => {
+        location.go('/authentication/signup');
+        expect(location.path()).toEqual('/authentication/signup');
+    }));
+});
