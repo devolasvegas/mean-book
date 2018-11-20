fa87ecf76ff0107966583c07c0c6c48fe5523e46
devolasvegas <devondaviau@yahoo.ca>
2018-11-19 22:00:19 -0500

    create routes testing file

---
+++ public/app/app.routes.spec.ts
@@ -0,0 +1,6 @@
+import { async, fakeAsync, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SpyLocation } from '@angular/common/testing';
+import { Location } from '@angular/common';
+import { AppModule } from './app.module';
+import { AppComponent } from './app.component';
